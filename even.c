e54e2389e35d8cf6ef591310f52f6b2665cb18de
MylesWatkinson <myleswatkinson1@gmail.com>
2023-08-07 19:00:07 +0930

    Debugginh

--- even.c
+++ even.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <time.h>
 
 #define SIGHUP 1
 
@@ -19,6 +20,16 @@ void hupHandler(int dummy) {
 int main()
 {
 
+    struct sigaction intSa;
+    intSa.sa_handler = intHandler;
+    sigemptyset(&intSa.sa_mask);
+    intSa.sa_flags = SA_RESTART; /* Restart functions if
+                                 interrupted by handler */
+    if (sigaction(SIGINT, &intSa, NULL) == -1)
+    {
+        printf("Error handlign signal");
+    }
+
     signal(SIGINT, intHandler);
     signal(SIGHUP, hupHandler);
 
@@ -30,7 +41,11 @@ int main()
     for (int i = 0;i<n*2;i+=2)
     {
         printf("%d\n", i);
-        sleep(5);
+        time_t endwait;
+        int seconds = 5;
+
+        endwait = time(NULL) + seconds;
+        while (time (NULL) < endwait) {}
     }
 
     return 0;
