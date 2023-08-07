4214b7b7477d595c8ace66034615a13a1f88aa25
MylesWatkinson <myleswatkinson1@gmail.com>
2023-08-07 19:38:30 +0930

    FIxed maybe

--- even.c
+++ even.c
@@ -19,42 +19,20 @@ void hupHandler(int dummy) {
 
 int main()
 {
-
-    // struct sigaction intSa;
-    // intSa.sa_handler = intHandler;
-    // sigemptyset(&intSa.sa_mask);
-    // intSa.sa_flags = SA_RESTART; /* Restart functions if
-    //                              interrupted by handler */
-    // if (sigaction(SIGINT, &intSa, NULL) == -1)
-    // {
-    //     printf("Error handlign signal");
-    // }
-
     signal(SIGINT, intHandler);
     signal(SIGHUP, hupHandler);
 
     int n;
 
-    printf("Input n value: ");
     scanf("%d", &n);
+
+    int i = 0;
 
     for (int i = 0;i<n*2;i+=2)
     {
-        printf("%d\n", i);
-        //sleep(5);
-        // time_t endwait;
-        // int seconds = 5;
-
-        // endwait = time(NULL) + seconds;
-        // while (time (NULL) < endwait) {}
-        time_t start_time = time(NULL);
-        int seconds_slept = 0;
-        int seconds = 5;
-        while (seconds_slept < seconds)
-        {
-            sleep(seconds - seconds_slept);
-            seconds_slept = (time(NULL) - start_time);
-        }
+        printf("%d", i);
+        fflush(stdout);
+        sleep(5);
     }
 
     return 0;
