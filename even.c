fa171156b6a1cc2e881b8035046a04a5a46be0ef
MylesWatkinson <myleswatkinson1@gmail.com>
2023-08-07 19:56:31 +0930

    fixing

--- even.c
+++ even.c
@@ -7,12 +7,12 @@
 #define SIGHUP 1
 
 void intHandler(int dummy) {
-    printf("Yeah!");
+    printf("Yeah!\n");
     fflush(stdout);
 }
 
 void hupHandler(int dummy) {
-    printf("Ouch!");
+    printf("Ouch!\n");
     fflush(stdout);
 }
 
@@ -28,16 +28,17 @@ int main()
 
     for (int i = 0;i<n*2;i+=2)
     {
-        printf("%d", i);
+        printf("%d\n", i);
         fflush(stdout);
-        time_t start_time = time(NULL);
-        int seconds_slept = 0;
-        int seconds = 5;
-        while (seconds_slept < seconds)
-        {
-            sleep(seconds - seconds_slept);
-            seconds_slept = (time(NULL) - start_time);
-        }
+        // time_t start_time = time(NULL);
+        // int seconds_slept = 0;
+        // int seconds = 5;
+        // while (seconds_slept < seconds)
+        // {
+        //     sleep(seconds - seconds_slept);
+        //     seconds_slept = (time(NULL) - start_time);
+        // }
+        sleep(5);
     }
 
     return 0;
